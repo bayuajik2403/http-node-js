8ae0ab2c08350040553a00eaaa556469762fc7ce
bayuajik2403 <bayuajik2403@gmail.com>
2020-10-22 00:22:46 +0700

    middleware in express

---
+++ express-middleware.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+const port = 8081
+
+app.use('/cap', express.static(__dirname + '/public/'))
+
+app.use((req,res,next)=>{
+    console.log('MIDDLEWARE')
+    next()
+})
+
+app.get('/', (req, res) => {
+
+    res.send(`
+    
+    <!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/cap/style.css">
+    <title>Document</title>
+</head>
+<body>
+
+
+    <h1>Hello</h1>
+
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Assumenda hic libero maxime necessitatibus, quaerat tempora
+        temporibus unde ut vel velit. Maiores molestiae necessitatibus
+        odio quia quod sequi, ut vitae voluptate.
+    </p>
+
+
+</body>
+</html>
+    
+    `)
+
+})
+app.listen(port, () => console.log(`Example app listening on port port!`))
