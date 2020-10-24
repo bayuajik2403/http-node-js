916faf6ad76d7ef05166460784995b03730eb35d
bayuajik2403 <bayuajik2403@gmail.com>
2020-10-24 11:29:26 +0700

    add send message to multiple client

--- websocket-server.js
+++ websocket-server.js
@@ -5,6 +5,10 @@ WSS.on('connection',(ws)=>{
     console.log('connected to websock nodejs');
 
     ws.on('message',(message)=>{
+        WSS.clients.forEach((client)=>{
+            client.send(message);
+        });
         console.log(message);
+        
     });
 });
